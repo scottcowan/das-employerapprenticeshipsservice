4645627a37096cf187fbb96e4ead4679590ef179
ijrussell <i.j.russell@tesco.net>
2016-08-18 09:29:17 +0100

    Simplest negative cases

---
+++ src/SFA.DAS.LevyAggregationProvider.Worker.UnitTests/FakeMessage.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EmployerApprenticeshipsService.Application.Messages;
+using SFA.DAS.Messaging;
+
+namespace SFA.DAS.LevyAggregationProvider.Worker.UnitTests
+{
+    public class FakeMessage : Message<EmployerRefreshLevyQueueMessage>
+    {
+        public FakeMessage(EmployerRefreshLevyQueueMessage content) : base(content)
+        {
+            
+        }
+
+        public override Task CompleteAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Task AbortAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/SFA.DAS.LevyAggregationProvider.Worker.UnitTests/LevyAggregationManagerTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Moq;
+using NLog;
+using NUnit.Framework;
+using SFA.DAS.EmployerApprenticeshipsService.Application.Messages;
+using SFA.DAS.LevyAggregationProvider.Worker.Providers;
+using SFA.DAS.LevyAggregationProvider.Worker.Queries.GetLevyDeclaration;
+using SFA.DAS.Messaging;
+using SFA.DAS.Messaging.FileSystem;
+
+namespace SFA.DAS.LevyAggregationProvider.Worker.UnitTests
+{
+    [TestFixture]
+    public class LevyAggregationManagerTests
+    {
+        private Mock<IPollingMessageReceiver> _pollingMessageReceiver;
+        private Mock<IMediator> _mediator;
+        private Mock<ILogger> _logger;
+        private LevyAggregationManager _manager;
+        private FakeMessage _message;
+
+        [SetUp]
+        public void Setup()
+        {
+            _pollingMessageReceiver = new Mock<IPollingMessageReceiver>();
+            _mediator = new Mock<IMediator>();
+            _logger = new Mock<ILogger>();
+            _manager = new LevyAggregationManager(_pollingMessageReceiver.Object, _mediator.Object, _logger.Object);
+
+            _message = new FakeMessage(new EmployerRefreshLevyQueueMessage
+            {
+                AccountId = 1
+            });
+        }
+
+        [Test]
+        public async Task MessageDoesNotIncludeAccount()
+        {
+            _message = new FakeMessage(new EmployerRefreshLevyQueueMessage());
+
+            _pollingMessageReceiver.Setup(x => x.ReceiveAsAsync<EmployerRefreshLevyQueueMessage>())
+                .ReturnsAsync(_message);
+
+            await _manager.Process();
+
+            _mediator.Verify(x => x.SendAsync(It.IsAny<GetLevyDeclarationRequest>()), Times.Never);
+        }
+
+        [Test]
+        public async Task MessageDoesHasAccountIdOfZero()
+        {
+            _message = new FakeMessage(new EmployerRefreshLevyQueueMessage
+            {
+                AccountId = 0
+            });
+
+            _pollingMessageReceiver.Setup(x => x.ReceiveAsAsync<EmployerRefreshLevyQueueMessage>())
+                .ReturnsAsync(_message);
+
+            await _manager.Process();
+
+            _mediator.Verify(x => x.SendAsync(It.IsAny<GetLevyDeclarationRequest>()), Times.Never);
+        }
+    }
+}
